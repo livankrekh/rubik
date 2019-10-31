c7df34cc3bc54ed5f3ef0100286363218b3b08bf
Paul Tyshevskyi <ptyshevs@gmail.com>
2019-10-31 20:42:54 +0200

    Moved astar solver to function

--- astar_solve.py
+++ astar_solve.py
@@ -42,20 +42,10 @@ def h_euclidean(candidate, solution):
             accum += (v_cand.v - v_sol.v) ** 2
     return accum
 
-if __name__ == '__main__':
-    cubik = Cubik()
+def astar_solve(cubik, target, max_iter=float('inf'), max_time=None, g_coef=.4, heuristic=h_manhattan, verbose=False, verbose_step=20):
+    # cubik = cubik.copy()  # Don't work properly if uncommented
+    cubik.heur = heuristic(cubik, target)
 
-    solution = cubik.copy()
-
-    cubik.apply_moves(cubik.parse_moves("R D"))
-
-    # print(cubik.repr(color=True))
-    # print(h_mismatch(cubik, solution))
-    # print(h_manhattan(cubik, solution))
-    # print(h_euclidean(cubik, solution))
-
-    h = h_euclidean
-    cubik.heur = h(cubik, solution)
     closed = set()
     opened = PriorityQueue()
     opened.add(cubik.heur, cubik)
@@ -68,10 +58,6 @@ if __name__ == '__main__':
     time_start = time.time()
     time_end = None if max_time is None else time_start + max_time
     iter = 0
-    max_iter = float('inf')
-    g_coef = 400
-    verbose = True
-    verbose_step = 20
     while True:
         if success:
             break
@@ -83,14 +69,14 @@ if __name__ == '__main__':
             break
         e = opened.pop()
         closed.add(e)
-        if e == solution:
+        if e == target:
             success = True
         else:
-            for move in solution.move_map.keys():
+            for move in target.move_map.keys():
                 candidate = e.copy()
                 hash_before = candidate.hash
                 candidate.apply_moves([move])
-                candidate.heur = h(candidate, solution)
+                candidate.heur = heuristic(candidate, target)
                 candidate.came_from = e.came_from + [move]
                 candidate.g = e.g + 1
 
@@ -100,6 +86,16 @@ if __name__ == '__main__':
         if verbose and iter % verbose_step == 0:
             print(f"[{iter}]: n_opened = {len(opened)} | n_closed = {len(closed)} | cur_depth: {e.g} | top-5 heur: {', '.join(str(round(_, 1)) for _ in opened.k[:5])}")
     
-    state_path = []
-    if success:
-        print(' '.join(e.came_from))
+    return success, e, e.came_from[:]
+
+if __name__ == '__main__':
+    cubik = Cubik()
+
+    solution = cubik.copy()
+
+    cubik.apply_moves(cubik.parse_moves("R D"))
+
+    success, result_state, path = astar_solve(cubik, solution, verbose=True)
+    print('SUCCESS:', success)
+    # print(result_state.repr())
+    print("Path:", path)
